cd0daeaa177d75d74f6778eccdff98d1d63ab05f
cfgong <crystalgong123@gmail.com>
2018-11-08 22:05:05 -0600

    added correct path

--- VideoToImages.m
+++ VideoToImages.m
@@ -1,12 +1,29 @@
-path = "C:\Users\cfg9740\Documents\Northwestern\EA3\Demo2\";
-v = VideoReader(path + "IMG_4242.MOV");
-NumberOfFrames = v.CurrentTime;
-% 14247
+% replace path with your current folder path 
+currPath = "C:\Users\cfg9740\Documents\Northwestern\EA3\Demo2\images\";
 
-for k = 1 : 20  %fill in the appropriate number
-  this_frame = readFrame(v);
-  thisfig = figure();
-  thisax = axes('Parent', thisfig);
-  image(this_frame, 'Parent', thisax);
-  title(thisax, sprintf('Frame #%d', k));
+v = VideoReader(path + "IMG_4242.MOV");
+frames = v.NumberOfFrames;
+v = VideoReader(path + "IMG_4242.MOV");
+% start step end
+frameNum = 0;
+step = floor(frames / 10); % take 10 frames
+for f = 1 : step : frames  
+    v.CurrentTime = v.Duration/frames*f;
+    currFrame = readFrame(v);
+    % write image frame to output file 
+    outputFile = frameNum + ".png";
+    imwrite(currFrame, fullfile(currPath, outputFile));
+    % incremenet frame number
+    frameNum = frameNum + 1;
+end
+
+% crop all the images 
+
+for frame = 1 : frameNum -1
+    I = imread(currPath + frame + ".png");
+    % imcrop rect [xmin ymin width height]
+    % set crop
+    I2 = imcrop(I,[250 525 550 550]);
+    outputFile = currPath + "cropped_" +frame + ".png";
+    imwrite(I2, outputFile);
 end
