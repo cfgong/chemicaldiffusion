972627f55a3a8347c30b7bc3e694290b0413d9b5
cfgong <crystalgong123@gmail.com>
2018-11-09 10:44:55 -0600

    added stdev plot

--- GetColorDistribution.m
+++ GetColorDistribution.m
@@ -3,6 +3,7 @@ currPath = "C:\Users\cfg9740\Documents\Northwestern\EA3\Demo2\images\";
 img = "cropped_";
 % iterate over 10 cropped images
 imgCount = 10;
+stdevs = zeros(1, imgCount);
 variances = zeros(1, imgCount);
 for i = 1 : imgCount
     image = imread(currPath + img + i + '.png');
@@ -23,7 +24,8 @@ for i = 1 : imgCount
     row = grayImage(250,:);
     % stdev is the square root of variance
     stdev = std(row, 1);
-    variances(i) = stdev;
+    stdevs(i) = stdev;
+    variances(i) = stdev.^2;
 end
 
 % plotting
@@ -34,3 +36,12 @@ ylabel('variance');
 
 % save plot as a png
 saveas(fig, currPath + 'variancesplot.png')
+
+% plotting
+fig = plot(stdevs);
+title('Row intensity stdevs');
+xlabel('frame');
+ylabel('stdev');
+
+% save plot as a png
+saveas(fig, currPath + 'stdevsplot.png')
